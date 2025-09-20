25aa8fbbaac318c58cb406a1138177810e40c107
Areeb455 <areebimam466@gmail.com>
2025-09-20 13:11:13 +0530

    ft - minor changes in scan screen

--- app/src/main/java/com/cattlelabs/cattleapp/ui/screens/CattleScannerScreen.kt
+++ app/src/main/java/com/cattlelabs/cattleapp/ui/screens/CattleScannerScreen.kt
@@ -6,19 +6,31 @@ import android.net.Uri
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Image
+import androidx.compose.material.icons.filled.PhotoCamera
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,11 +38,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
+import com.cattlelabs.cattleapp.R
 import com.cattlelabs.cattleapp.navigation.CattleAppScreens
+import com.cattlelabs.cattleapp.ui.theme.Green
+import com.cattlelabs.cattleapp.ui.theme.LightGreen
 import com.cattlelabs.cattleapp.ui.util.createImageUri
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -73,45 +92,92 @@ fun CattleScannerScreen(
             }
         }
     )
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(R.drawable.bg1),
+            contentDescription = "Background",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop,
+        )
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("Scan Cattle") },
+                    colors = TopAppBarDefaults.topAppBarColors(
 
-    Scaffold(
-        topBar = { TopAppBar(title = { Text("Scan Cattle") }) }
-    ) { paddingValues ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Button(
-                onClick = {
-                    val permission =
-                        ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
-                    if (permission == PackageManager.PERMISSION_GRANTED) {
-                        val uri = createImageUri(context)
-                        imageUri = uri
-                        cameraLauncher.launch(uri)
-                    } else {
-                        permissionLauncher.launch(Manifest.permission.CAMERA)
+                        containerColor = Color.Transparent // Make TopAppBar transparent
+                    )
+                )
+            },  containerColor = Color.Transparent
+
+        ) { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Button(
+                    onClick = {
+                        val permission =
+                            ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+                        if (permission == PackageManager.PERMISSION_GRANTED) {
+                            val uri = createImageUri(context)
+                            imageUri = uri
+                            cameraLauncher.launch(uri)
+                        } else {
+                            permissionLauncher.launch(Manifest.permission.CAMERA)
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(55.dp),
+                    shape = RoundedCornerShape(12.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Green,
+                        contentColor = Color.White
+                    )
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Icon(
+                            imageVector = Icons.Default.PhotoCamera,
+                            contentDescription = "Open Camera"
+                        )
+                        Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+                        Text("Open Camera", fontWeight = FontWeight.Bold)
                     }
-                },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text("Open Camera")
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                OutlinedButton(
+                    onClick = {
+                        galleryLauncher.launch("image/*")
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(55.dp),
+                    shape = RoundedCornerShape(12.dp),
+                    colors = ButtonDefaults.outlinedButtonColors(
+                        containerColor = Green,
+                        contentColor = Color.White
+
+                    ),
+                    border = BorderStroke(1.dp, Color.Green)
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Icon(
+                            imageVector = Icons.Default.Image,
+                            contentDescription = "Choose from Gallery"
+                        )
+                        Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+                        Text("Choose from Gallery", fontWeight = FontWeight.Bold)
+                    }
+                }
             }
 
-            Spacer(modifier = Modifier.height(16.dp))
-
-            OutlinedButton(
-                onClick = {
-                    galleryLauncher.launch("image/*")
-                },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text("Choose from Gallery")
-            }
         }
     }
 }
